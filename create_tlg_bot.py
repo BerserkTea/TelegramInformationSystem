ca1f00a4ac07142b2595534fdc92f3c9b26af1a4
«AndronovIvan» <andronoviv4@gmail.com>
2022-04-15 12:08:43 +0300

    added gitignore files, usefullinks, spy, create_bot, edit readme

--- create_tlg_bot.py
+++ create_tlg_bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
 
-updater = Updater('5331187324:AAGU7esB7TQvG3r8i8B963HaPFPW7U-v52I')
+updater = Updater('')
 
 updater.dispatcher.add_handler(CommandHandler('start', greeting_command))   
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
--- spy.py
+++ spy.py
@@ -3,6 +3,6 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 
 
 def log(update: Update, context: CallbackContext):
-    file = open ('db.csv','a')
+    file = open ('db.csv','a', encoding='UTF-8')
     file.write (f'{update.effective_user.first_name}, {update.effective_user.id}, {update.message.text}\n ')
     file.close()
