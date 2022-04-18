d81e666521e2f25cd5c2351189c0ce2a608b75e2
«AndronovIvan» <andronoviv4@gmail.com>
2022-04-18 16:39:38 +0300

    clear bot key

--- create_tlg_bot.py
+++ create_tlg_bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
 from bot_link_com import *
 
-updater = Updater('5334748744:AAGZuS8H575cbDURtkt6qX9eRHGiUGB8yMA')
+updater = Updater('')
 
 updater.dispatcher.add_handler(CommandHandler('start', greeting_command))   
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
